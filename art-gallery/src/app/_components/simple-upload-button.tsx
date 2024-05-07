6987c7f117b748f7b860905f11d406bb18d682c7
Exploser <exploserbusiness@gmail.com>
2024-05-07 19:20:25 -0400

    Add spinner for uploading toast

--- art-gallery/src/app/_components/simple-upload-button.tsx
+++ art-gallery/src/app/_components/simple-upload-button.tsx
@@ -8,45 +8,63 @@ import { useUploadThing } from "~/utils/uploadthing";
 type Input = Parameters<typeof useUploadThing>;
 
 const useUploadThingInputProps = (...args: Input) => {
-  const $ut = useUploadThing(...args);
+    const $ut = useUploadThing(...args);
 
-  const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) return;
+    const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!e.target.files) return;
 
-    const selectedFiles = Array.from(e.target.files);
-    const result = await $ut.startUpload(selectedFiles);
+        const selectedFiles = Array.from(e.target.files);
+        const result = await $ut.startUpload(selectedFiles);
 
-    console.log("uploaded files", result);
-    // TODO: persist result in state maybe?
-  };
+        console.log("uploaded files", result);
+        // TODO: persist result in state maybe?
+    };
 
-  return {
-    inputProps: {
-      onChange,
-      multiple: ($ut.permittedFileInfo?.config?.image?.maxFileCount ?? 1) > 1,
-      accept: "image/*",
-    },
-    isUploading: $ut.isUploading,
-  };
+    return {
+        inputProps: {
+            onChange,
+            multiple: ($ut.permittedFileInfo?.config?.image?.maxFileCount ?? 1) > 1,
+            accept: "image/*",
+        },
+        isUploading: $ut.isUploading,
+    };
 };
 
 function UploadSVG() {
     return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
-    </svg>
-  )
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+        </svg>
+    )
 }
 
-export function SimpleUploadButton(){
+function LoadingSpinnerSVG() {
+    return (
+    <svg 
+        width="24" 
+        height="24" 
+        stroke="#aaa" 
+        viewBox="0 0 24 24" 
+        xmlns="http://www.w3.org/2000/svg"
+    >
+    <g className="spinner_V8m1">
+    <circle cx="12" cy="12" r="9.5" fill="none" stroke-width="3"></circle></g></svg>)
+}
+
+export function SimpleUploadButton() {
     const router = useRouter();
 
     const { inputProps } = useUploadThingInputProps("imageUploader", {
         onUploadBegin() {
-            toast("Uploading...", {
-                duration: 100000,
-                id: "upload-begin",
-            });
+            toast(
+                <div className="flex gap-2 text-white items-center">
+                    <LoadingSpinnerSVG /> <span className="text-lg">Uploading...</span> 
+                </div>,
+                {
+                    duration: 100000,
+                    id: "upload-begin",
+                },
+            );
         },
         onClientUploadComplete() {
             toast.dismiss("upload-begin");
@@ -57,15 +75,15 @@ export function SimpleUploadButton(){
         }
     });
 
-    return( 
-    <div>
-        <label htmlFor="upload-button" className="cursor-pointer"><UploadSVG /></label>
-        <input 
-            id="upload-button"
-            type="file"
-            className="sr-only"
-            {...inputProps}
-        />
-    </div>
-);
+    return (
+        <div>
+            <label htmlFor="upload-button" className="cursor-pointer"><UploadSVG /></label>
+            <input
+                id="upload-button"
+                type="file"
+                className="sr-only"
+                {...inputProps}
+            />
+        </div>
+    );
 }
